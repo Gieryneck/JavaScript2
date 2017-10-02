ecaf8482e78813873eb6ad06860d17155d789ab3
Gieryn <gierynt@gmail.com>
2017-10-03 00:05:05 +0200

    not working

---
+++ gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function(grunt) {
+  
+  // Project configuration
+  grunt.initConfig({
+    sass: {
+
+        options: {
+
+            sourceMap: true
+        },
+
+
+        dist: {
+
+            files: {
+              'css/style.css': 'sass/sass.sass'
+            }
+        }
+
+    },
+
+
+
+
+
+
+    jshint: {
+
+        all: ['gruntfile.js', 'js/*.js']
+    },
+
+
+
+    
+
+
+
+    watch: {
+
+        styles: {
+
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },        
+        },
+
+        scripts: {
+
+            files: ['gruntfile.js', 'js/*.js'],
+            tasks: ['grunt-contrib-jshint'],
+            options: {
+                spawn: false,
+          },
+        }
+
+    },
+
+
+    browserSync: {
+        dev: {  
+            bsFiles: {
+                src: [
+                    'js/*.js',
+                    'css/*.css',
+                    '*.html'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: './'
+            }
+        }  
+    }
+
+  }); 
+
+
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+
+
+  // Default tasks
+  grunt.registerTask('default', ['sass', 'jshint', 'browserSync', 'watch']);
+
+
+};
---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// single line comment, mozna tez uzyc jako 
+//chwilowe wylaczenie fragmentu kodu
+
+
+/*
+
+bledy 
+mozna 
+sprawdzac 
+w 
+konsoli
+devtools
+
+Pamiętaj, że wystąpienie błędu zazwyczaj
+ przerywa wykonywanie skryptu - to znaczy, 
+ że kod wykona się albo do końca albo do momentu wykrycia błędu.
+
+Aby przetestować działanie kodu lub jego poprawność,
+ możesz go także po prostu wpisać w konsoli. 
+ Na przykład dowiesz się jaki jest wynik dzielenia 
+ przez zero według Twojej przeglądarki :)
+
+*/
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
